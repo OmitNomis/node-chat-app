22ee7b0b345e13d6bd222cf6449a6e105fa1997d
OmitNomis <simon98190@gmail.com>
2022-12-21 23:20:24 +0545

    changes to server side socket stuff

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require("express");
-const path = require("path");
-const app = express();
-const server = require("http").createServer(app);
-app.use(express.static(path.join(__dirname + "/public")));
+const http = require("http");
+const socketio = require("socket.io");
 
-server.listen(5000);
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("A user has connected");
+
+  socket.on("message", (message) => {
+    // Broadcast the message to all connected clients
+    io.emit("message", message);
+  });
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
