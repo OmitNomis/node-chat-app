1e2d912817cab756c345721f3f6218a25ad6a74b
OmitNomis <simon98190@gmail.com>
2022-12-21 23:22:25 +0545

    canged port to 5000

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
